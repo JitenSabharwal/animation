a8b87e9fd72ffe06d534430721fa5490451ec299
JitenSabharwal <jitensabharwal08@gmail.com>
2017-12-20 04:33:58 +0530

    ading react component strcutre

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Image,
+  Easing
+} from 'react-native'
+
+ class App extends Component {
+  render() {
+    return (
+     
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
+export default App
