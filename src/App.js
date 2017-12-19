0d347b0be5b2be3b0bfcf7a14e880021a5afbf2e
JitenSabharwal <jitensabharwal08@gmail.com>
2017-12-20 03:50:41 +0530

    spin animation added

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Image,
+  Easing
+} from 'react-native'
+class App extends Component {
+  spinValue= new Animated.Value(0) 
+  componentDidMount() {
+    this.spin()
+  }
+  spin() {
+    this.spinValue.setValue(0)
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 1,
+        duration: 4000,
+        easing: Easing.linear
+      }
+    ).start(() => this.spin())
+  }
+  render() {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    })
+    return (
+      <View style={styles.container}>
+        <Animated.Image
+          style={{
+            width: 227,
+            height: 200,
+            transform: [{rotate: spin}] }}
+            source={{uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png'}}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default App
